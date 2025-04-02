2f6a85f745604ae7fd5670d32d91bfd8e8bfbf91
falbertoli <falbertoli3@gatech.edu>
2025-04-01 22:05:45 -0400

    delete model folder

--- backend/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# backend/models/__init__.py
-from .facility import Facility
-from .regulation import Regulation
-from .storage import StorageConfiguration
-
-__all__ = ['Facility', 'Regulation', 'StorageConfiguration']
